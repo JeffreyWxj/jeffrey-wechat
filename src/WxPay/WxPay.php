cf05df8f0a9f961c94033ccf3afd5c1daf2c54e5
Jeffrey <1737723363@qq.com>
2017-12-08 20:26:37 +0800

    package init && dev package

---
+++ src/WxPay/WxPay.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 微信支付核心类
+ * Created by PhpStorm.
+ * User: Jeffrey
+ * Date: 2017/12/8
+ * Time: 20:20
+ */
+
+namespace WxPay;
+
+class WxPay
+{
+  public function demo()
+  {
+    echo "demo";
+  }
+}
